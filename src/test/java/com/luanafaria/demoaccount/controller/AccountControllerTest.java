41d3bb3c43dc071567ef1c9b78e294eb98afc564
Luana Faria <luanacostafaria@gmail.comm>
2023-01-30 18:49:35 -0300

    Optimized imports

--- src/test/java/com/luanafaria/demoaccount/controller/AccountControllerTest.java
+++ src/test/java/com/luanafaria/demoaccount/controller/AccountControllerTest.java
@@ -2,10 +2,8 @@ package com.luanafaria.demoaccount.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luanafaria.demoaccount.DemoBankAccountApiApplication;
-import com.luanafaria.demoaccount.entity.Account;
 import com.luanafaria.demoaccount.payload.AccountDto;
 import com.luanafaria.demoaccount.repository.AccountRepository;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
--- src/test/java/com/luanafaria/demoaccount/controller/TransactionControllerTest.java
+++ src/test/java/com/luanafaria/demoaccount/controller/TransactionControllerTest.java
@@ -2,22 +2,12 @@ package com.luanafaria.demoaccount.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luanafaria.demoaccount.DemoBankAccountApiApplication;
-import com.luanafaria.demoaccount.entity.Account;
-import com.luanafaria.demoaccount.payload.TransactionDto;
 import com.luanafaria.demoaccount.repository.AccountRepository;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.math.BigDecimal;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = DemoBankAccountApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
